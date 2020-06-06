e784a1b0a301c2c28022babbebc9220661234a7e
unknown <simeyonk@gmail.com>
2020-06-06 18:52:13 +0700

    source code tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  theme: {
+    extend: {},
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus','active', 'visited '],
+	opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    textColor: ['responsive', 'hover', 'focus', 'visited','group-hover'],
+	  
+  },
+  plugins: [],
+}
